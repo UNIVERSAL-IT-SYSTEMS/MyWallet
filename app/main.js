8a88dd9560ac27352ee34a249c72136bbbe468ae
sree-bh <sritama.imps@gmail.com>
2015-11-16 17:33:31 +0530

    initial commit

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ requirejs.config({
 
     paths: {
         angular: '../node_modules/angular/angular.min',
+//        uiRouter: 'angular-ui-router',
         mwCtrl: 'login/mwController',
         mwDir: 'login/mwDirective',
         mwService: 'login/mwService',
@@ -11,20 +12,38 @@ requirejs.config({
     shim: {
         angular: {
             exports: 'angular'
-        }
+        },
+//        uiRouter: {
+//            deps: ['angular'],
+//            exports: 'ui-router'
+//        }
     },
 
     //deps: ['./bootstrap']
 
 });
 
-require(['angular', 'mwCtrl', 'mwDir', 'mwService'], function() {
+require(['angular', /*'uiRouter',*/ 'mwCtrl', 'mwDir', 'mwService'], function() {
 	'use strict';
 
 	angular
 	    .module('myWallet', [
+//			'ui-router',
 			'mw-ctrl',
 			'mw-widget',
 			'mw-service'
         ]);
+
+//	angular
+//    .module('myWallet')
+//        .config(function($routeProvider) {
+//	        $routeProvider
+//	
+//	            // route for the home page
+//	            .when('/', {
+//	                templateUrl : 'login.html',
+//	                controller  : 'mwCtrl',
+//	                controllerAs: 'loginC'
+//	            });
+//	    });
 });
